b1fea574ec867331a0366ba1959fa276dbe47dab
elizaveta.bredikhina <bredikhina_ea@mail.ru>
2026-03-03 14:23:38 +0300

    commit 6

--- src/main/java/com/itq/document_management_service/service/DocumentUtilService.java
+++ src/main/java/com/itq/document_management_service/service/DocumentUtilService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.stream.IntStream.range;
 
@@ -23,7 +24,10 @@ public class DocumentUtilService {
     public void generateDocuments() {
         log.info("Обработка запроса на генерацию {} документов через утилиту", properties.getBatchSize());
 
+        AtomicInteger createdCount = new AtomicInteger();
+
         range(0, properties.getBatchSize()).forEach(iterationNumber -> {
+
             CreateDocumentMetadataDto createDocumentMetadataDto = new CreateDocumentMetadataDto(
                     properties.getCreatedBy(),
                     properties.getTitle() + UUID.randomUUID()
@@ -32,11 +36,22 @@ public class DocumentUtilService {
             try {
                 log.info("Создаётся документ с названием {}", createDocumentMetadataDto.getTitle());
                 documentProcessingService.createDocument(createDocumentMetadataDto);
+                createdCount.getAndIncrement();
+
                 log.info("Документ с названием {} успешно создан", createDocumentMetadataDto.getTitle());
+                logProgress(properties.getBatchSize(), iterationNumber);
             } catch (Exception e) {
                 log.error("Документ с названием {} не может быть создан по причине : {}", createDocumentMetadataDto.getTitle(), e.getMessage());
             }
         });
-        log.info("Обработка запроса на генерацию {} документов через утилиту завершена", properties.getBatchSize());
+        log.info("Обработка запроса на генерацию {} документов через утилиту завершена, успешно создано {}",
+                properties.getBatchSize(), createdCount);
+    }
+
+    private void logProgress(Integer batchSize, int iterationNumber) {
+        if (iterationNumber == batchSize - 1 || (iterationNumber + 1) % (batchSize / 10) == 0) {
+            int percent = (int) ((iterationNumber + 1) * 100.0 / batchSize);
+            log.info("Создано {} из {} документов ({}%)", iterationNumber + 1, batchSize, percent);
+        }
     }
 }
